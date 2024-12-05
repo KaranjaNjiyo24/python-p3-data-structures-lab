1df891e3edcf4fffa37247ff9f7005c1129ac6a8
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-05 07:41:02 +0300

    Getting names of each spicy food

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,8 +17,8 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
-
+    return[food['name'] for food in spicy_foods]
+# print(get_names(spicy_foods))
 def get_spiciest_foods(spicy_foods):
     pass
 
